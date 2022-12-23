017f243920a86a66b19c3fffe9145ec2b2c2fc5c
Shubham Anand Gupta <81542648+shubhattin@users.noreply.github.com>
2022-12-23 13:05:50 +0530

    added markdown translation support

--- src/langs/anuvad.py
+++ src/langs/anuvad.py
@@ -1,6 +1,6 @@
 from time import time
 import shubhlipi as sh, json, yaml, os, re
-import markdown, markdownify
+import markdown, markdownify, jinja2
 
 ln = sh.lang_list
 ln2 = sh.dict_rev(ln)
@@ -13,23 +13,74 @@ else:
 if input(f"Do you want to translate with {src} as base? ") != "yes":
     exit()
 
-sh.write("locales.json", sh.dump_json(sh.dict_rev(sh.lang_list), 2))
-sh.start_thread(lambda: sh.cmd("npx prettier locales.json --write", display=False))
+DEFAULT_LOCALE = "en"
+sh.write(
+    "locales.json",
+    sh.dump_json(
+        dict(locales=sh.dict_rev(sh.lang_list), default_locale=DEFAULT_LOCALE), 2
+    ),
+)
+sh.prettier_beautify("locales.json")
 main_db = yaml.safe_load(sh.read(f"data/{ln2[src]}.yaml"))
 anu = {}
+
+
+def render_from_string(template: str, **data):
+    return (
+        jinja2.Environment(loader=jinja2.BaseLoader)
+        .from_string(template)
+        .render(**data)
+    )
+
+
+TEMP = r"""{% for x in DATA -%}
+# {{x|safe}}
+
+{{DATA[x]|safe}}
+
+{% endfor -%}"""
+
+
+def get_all_lang_markdown():
+    res = {}
+    MATCHES = re.compile(r"# .+?\n\n.+?\n\n", re.DOTALL)
+    HEAD = re.compile(r"(?<=# ).+?(?=\n)")
+    LEKH = re.compile(r"(?<=\n\n).+?(?=\n\n)", re.DOTALL)
+    for lng in list(sh.lang_list_names.keys()):
+        code = sh.lang_list[lng]
+        res[code] = {}
+        vl = sh.read(f"data/markdown/{lng}.md") + "\n"
+        for x in MATCHES.findall(vl):
+            head = HEAD.findall(x)[0]
+            lekh = LEKH.findall(x)[0]
+            res[code][head] = lekh
+    return res
+
+
+mark_db = get_all_lang_markdown()
+
 only = json.loads(sh.read("r.json"))
 print(only)
 
 
 def pre_process(func=0):
     func = [sh.anuvadak, sh.parivartak][func]
+    global mark_db
 
     def process_func(vl, src, to):
         vl = markdown.markdown(vl)
         if vl.startswith("<p>") and vl.endswith("</p>"):
             vl = vl[3:-4]
+        template_found = re.match("<.+?>", vl)
+        tmp = vl
+        templ_nm = None
+        if template_found:
+            templ_nm = vl[1:-1]
+            vl = markdown.markdown(mark_db[src if to != "ur" else "hi"][templ_nm])
         data = func(vl, src, to, html=True)
-        return markdownify.markdownify(data)
+        if template_found:
+            mark_db[to][templ_nm] = markdownify.markdownify(data)
+        return markdownify.markdownify(data) if not template_found else tmp
 
     return process_func
 
@@ -55,8 +106,8 @@ for y in ln:
         anu[y] = sh.process_json(
             anu["हिन्दी"],
             org,
-            "Hindi",
-            "Urdu",
+            "hi",
+            "ur",
             no=only["no"],
             yes=only["yes"],
             func=pre_process(1),
@@ -65,6 +116,8 @@ for y in ln:
         f"data/{y}.yaml",
         yaml.safe_dump(anu[y], allow_unicode=True, sort_keys=False),
     )
+    sh.write(f"data/markdown/{y}.md", render_from_string(TEMP, DATA=mark_db[ln[y]]))
+
     print(ln[y], f"{time()-tm}s")
 
 sh.start_thread(lambda: sh.prettier_beautify("data"))
--- src/langs/model.ts
+++ src/langs/model.ts
@@ -1,13 +1,1 @@
-export interface dattType {
-  main: {
-    title: [string, string];
-    description: string;
-    features: {
-      name: string;
-      lekhan_sahayika: { title: string; lekh: string };
-      phonetic_table: { title: string; lekh: string };
-      brahmic_support: { title: string; lekh: string };
-      parivartak: { title: string; lekh: string };
-    };
-  };
-}
+The system cannot find the file specified.
